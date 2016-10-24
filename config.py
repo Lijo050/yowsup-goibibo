4e8206643ac5c7e054e3db771d7b8f8027a75392
Lijo Johny <lijojohny05@gmail.com>
2016-10-24 13:12:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,12 +8,12 @@
 # You now have the four necessary credentials. Copy the API key, API secret, 
 # Access token, and Access token secret here.
 # NOTE: API key and API secret go in the twitter_consumer_key and twitter_consumer_secret vars.
-#
 
-twitter_consumer_key = 'W4YE494z0hnnNgsyMfrQVoRgy'
-twitter_consumer_secret = 'fGL4tqw2JmdqpObLKxXIA7Tf3ZtWiAShnBGrdne1ibSTFsX3m2'
-twitter_access_token = '1693425644-piUDkYvBP6GugSMJzC85wvp3LkMm6l1loarE0EU'
-twitter_access_secret = 'MTu3O8VYySjwY2UbOVlcmYeV0OOjYZWLfDnVRqqWq80SS'
+#put twitter details
+twitter_consumer_key = ''
+twitter_consumer_secret = ''
+twitter_access_token = ''
+twitter_access_secret = ''
 
 # Personality Insights credentials and URL
 #
@@ -21,8 +21,9 @@ twitter_access_secret = 'MTu3O8VYySjwY2UbOVlcmYeV0OOjYZWLfDnVRqqWq80SS'
 # and clicking the "show credentials" link on the service in the application dashboard.
 # Or you can use "cf env <application name>" from the command line to get the credentials.
 
+#put Watson PI username and pwd
 pi_url = 'https://gateway.watsonplatform.net/personality-insights/api'
-pi_username = '5854ca6c-a2c5-437c-85b1-cc0084beae90'
-pi_password = '1GpFejtVZgLu'
+pi_username = ''
+pi_password = ''
 
 
